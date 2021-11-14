30280bec6d9f8460c07763764413dcf89d2d38c5
Nilesh D <nileshlund@gmail.com>
2021-11-14 16:03:07 +0530

    :bulb: (fix): Update code comments

--- middleware/error.js
+++ middleware/error.js
@@ -14,13 +14,13 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
-  // Mongoose Duplicate Key - Is triggered when we try to insert a bootcamp with a duplicate name
+  // Mongoose Duplicate Key
   if (err.code === 11000) {
     const message = `Duplicate field value entered`;
     error = new ErrorResponse(message, 400);
   }
 
-  // Mongoose Validation Error - Is triggered when a required field in the model is not passed in req.body
+  // Mongoose Validation Error
   if (err.name === 'ValidationError') {
     const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
